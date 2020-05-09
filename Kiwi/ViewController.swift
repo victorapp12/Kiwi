a34847516c0d2bcadd371cbaa4429aaca69eef4f
victorapp12 <victor.perdomo12@gmail.com>
2020-05-09 03:13:32 +0200

    Initial Commit

---
+++ Kiwi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kiwi
+//
+//  Created by Victor Perdomo on 09/05/2020.
+//  Copyright © 2020 Victor Perdomo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
